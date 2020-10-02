f4cc90e6346ad22a583a377c72b6d13e695479ac
Vladimir Sazonov <v.sazonov@kefirgames.com>
2020-10-02 12:06:34 +0400

    add: attack button view

---
+++ Assets/Scripts/Players/Views/AttackButtonView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Players.Views
+{
+    public class AttackButtonView : MonoBehaviour, IAttackButtonView, IPointerClickHandler
+    {
+        public event Action Attack;
+
+        private void OnAttack()
+        {
+            Attack?.Invoke();
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            OnAttack();
+        }
+    }
+}
---
+++ Assets/Scripts/Players/Views/IAttackButtonView.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Players.Views
+{
+    public interface IAttackButtonView
+    {
+        event Action Attack;
+    }
+}
