207ca914ab4db95b5473623d42332476ae00fdd1
Vladimir Sazonov <v.sazonov@kefirgames.com>
2020-10-21 13:37:20 +0400

    add: client proxy interface

---
+++ Server/Server/ServerCode/Network/IClientProxy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Server.Network
+{
+    public interface IClientProxy
+    {
+        int IdClient { get; }
+        Queue<byte> UnprocessedCommands { get; }
+        Queue<byte> NotSentPacketCommands { get; }
+    }
+}
