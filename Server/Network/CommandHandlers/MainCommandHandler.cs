5d4c5a4da17ec009df467e4a2289590817ceadc2
Vladimir Sazonov <v.sazonov@kefirgames.com>
2020-10-28 11:17:17 +0400

    add: main command handler

---
+++ Server/Network/CommandHandlers/MainCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Network.CommandHandlers
+{
+    public readonly struct MainCommandHandler : ICommandHandler
+    {
+        private readonly ICustomPacket _unprocessedReceivedPacket;
+
+        public MainCommandHandler(ICustomPacket unprocessedReceivedPacket)
+        {
+            _unprocessedReceivedPacket = unprocessedReceivedPacket;
+        }
+
+        public void HandleCommand()
+        {
+            GameCommandType commandType = _unprocessedReceivedPacket.Pull<GameCommandType>();
+            ICommandHandler commandHandler;
+
+            switch (commandType)
+            {
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            commandHandler.HandleCommand();
+        }
+    }
+}
