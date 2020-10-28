b6b99d1ebd532b1cd1c928445b483432415bc31e
Vladimir Sazonov <v.sazonov@kefirgames.com>
2020-10-28 11:18:15 +0400

    add: command packet handler

---
+++ Server/Network/PacketHandlers/CommandPacketHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Network.PacketHandlers
+{
+    public readonly struct CommandPacketHandler : IPacketHandler
+    {
+        private readonly uint _clientId;
+        private readonly byte[] _packet;
+        private readonly IDictionary<uint, IClientProxy> _clientProxyDic;
+
+        public CommandPacketHandler(in uint clientId, byte[] packet, IDictionary<uint, IClientProxy> clientProxyDic)
+        {
+            _clientId = clientId;
+            _packet = packet;
+            _clientProxyDic = clientProxyDic;
+        }
+
+        public void HandlePacket()
+        {
+            _clientProxyDic[_clientId].UnprocessedReceivedPacket.Fill(_packet);
+        }
+    }
+}
