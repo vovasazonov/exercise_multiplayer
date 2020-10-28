b582fe7828f665ff5c2bbb7005b2c421bac91923
Vladimir Sazonov <v.sazonov@kefirgames.com>
2020-10-28 17:44:40 +0400

    fix: absent parametr in constructor

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Models;
 using Network;
 using Serialization;
 using Serialization.BinaryFormatterSerialization;
@@ -7,11 +8,12 @@ class Program
 {
     static void Main(string[] args)
     {
+        IModelManager modelManager = new ModelManager();
         ISerializer serializer = new BinaryFormatterSerializer();
         var udpServerInfo = new LocalUdpServerInfoCreator().Create();
         using IServer server = new UdpServer(udpServerInfo);
-        using NetworkManager networkManager = new NetworkManager(server,serializer) {MillisecondsTickServer = 100};
-            
+        using NetworkManager networkManager = new NetworkManager(server, serializer, modelManager) {MillisecondsTickServer = 100};
+
         Console.WriteLine("Press 'q' to stop server");
         while (Console.ReadKey().Key != ConsoleKey.Q)
         {
