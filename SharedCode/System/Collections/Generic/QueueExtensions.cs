2ff6d0536fea325fcb537d4018f7d5929d5af849
Vladimir Sazonov <v.sazonov@kefirgames.com>
2020-10-28 11:12:34 +0400

    add: queue extension for bytes

---
+++ SharedCode/System/Collections/Generic/QueueExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace System.Collections.Generic
+{
+    internal static class QueueExtensions
+    {
+        public static void Enqueue(this Queue<byte> queueBytes, in byte[] bytes)
+        {
+            foreach (var @byte in bytes)
+            {
+                queueBytes.Enqueue(@byte);
+            }
+        }
+        
+        public static void DiscardFirst(this Queue<byte> queueBytes, long amountBytes)
+        {
+            for (int i = 0; i < amountBytes; i++)
+            {
+                queueBytes.Dequeue();
+            }
+        }
+    }
+}
