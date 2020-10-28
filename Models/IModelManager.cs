8f8758fe8b2778932e7b82391c586e54392b3fb4
Vladimir Sazonov <v.sazonov@kefirgames.com>
2020-10-28 17:31:18 +0400

    add: model manager

---
+++ Models/IModelManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Models.Characters;
+
+namespace Models
+{
+    public interface IModelManager
+    {
+        ITrackableDictionary<int,ICharacterModel> CharacterModelDic { get; }
+    }
+}
---
+++ Models/ModelManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Models.Characters;
+
+namespace Models
+{
+    public class ModelManager : IModelManager
+    {
+        public ITrackableDictionary<int, ICharacterModel> CharacterModelDic { get; } = new TrackableDictionary<int, ICharacterModel>();
+    }
+}
