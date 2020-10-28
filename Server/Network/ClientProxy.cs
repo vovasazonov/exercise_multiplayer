9877c3a6ec4f4e6f7846f5b25372ace7d1f9dc44
Vladimir Sazonov <v.sazonov@kefirgames.com>
2020-10-28 11:15:39 +0400

    add: client proxy class

---
+++ Server/Network/ClientProxy.cs
@@ -0,0 +1,19 @@
+﻿using Serialization;
+
+namespace Network
+{
+    public class ClientProxy : IClientProxy
+    {
+        public uint Id { get; }
+        public ICustomPacket UnprocessedReceivedPacket { get; }
+        public ICustomPacket NotSentToClientPacket { get; }
+
+        public ClientProxy(uint id, ISerializer serializer)
+        {
+            Id = id;
+            
+            UnprocessedReceivedPacket = new CustomPacket(serializer);
+            NotSentToClientPacket = new CustomPacket(serializer);
+        }
+    }
+}
---
+++ Server/Network/IClientProxy.cs
@@ -0,0 +1,9 @@
+﻿namespace Network
+{
+    public interface IClientProxy
+    {
+        uint Id { get; }
+        ICustomPacket UnprocessedReceivedPacket { get; }
+        ICustomPacket NotSentToClientPacket { get; }
+    }
+}
