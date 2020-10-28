6163a083cf1eddc2a7e61fd6c76fc7205cb66413
Vladimir Sazonov <v.sazonov@kefirgames.com>
2020-10-28 11:11:27 +0400

    add: trackable dictionary

---
+++ SharedCode/System/Collections/Generic/ITrackableDictionary.cs
@@ -0,0 +1,8 @@
+﻿namespace System.Collections.Generic
+{
+    public interface ITrackableDictionary<TKey,TValue> : IDictionary<TKey,TValue>
+    {
+        event Action<TKey, TValue> Adding;
+        event Action<TKey, TValue> Removing;
+    }
+}
---
+++ SharedCode/System/Collections/Generic/TrackableDictionary.cs
@@ -0,0 +1,67 @@
+﻿namespace System.Collections.Generic
+{
+    public class TrackableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ITrackableDictionary<TKey, TValue>
+    {
+        public event Action<TKey, TValue> Adding;
+        public event Action<TKey, TValue> Removing;
+
+        public TrackableDictionary()
+        {
+        }
+
+        public TrackableDictionary(int capacity) : base(capacity)
+        {
+        }
+
+        public new void Clear()
+        {
+            foreach (var key in Keys)
+            {
+                OnRemoving(key,base[key]);
+            }
+            
+            base.Clear();
+        }
+
+        public new void Add(TKey key, TValue value)
+        {
+            OnAdding(key, value);
+            base.Add(key,value);
+        }
+
+        public new bool Remove(TKey key)
+        {
+            if (TryGetValue(key, out var value))
+            {
+                OnRemoving(key,value);
+            }
+
+            return base.Remove(key);
+        }
+
+        public new TValue this[TKey key]
+        {
+            get => base[key];
+            set
+            {
+                if (ContainsKey(key))
+                {
+                    Remove(key);
+                }
+                
+                OnAdding(key,value);
+                base[key] = value;
+            }
+        }
+
+        protected virtual void OnAdding(TKey key, TValue value)
+        {
+            Adding?.Invoke(key, value);
+        }
+
+        protected virtual void OnRemoving(TKey key, TValue value)
+        {
+            Removing?.Invoke(key, value);
+        }
+    }
+}
