51366323a154d20dfa79d2687d1a6cf6215b79e3
Vladimir Sazonov <v.sazonov@kefirgames.com>
2020-10-28 11:15:03 +0400

    add: udp server info struct

---
+++ Server/Network/UdpServerInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Network
+{
+    public struct UdpServerInfo
+    {
+        public ushort Port;
+        public int MaxClients;
+        public byte ChannelId;
+        public uint PeerTimeOutLimit;
+        public uint PeerTimeOutMinimum;
+        public uint PeerTimeOutMaximum;
+    }
+}
