84396e04465358799eb4a3a31ab376259306ae37
Vladimir Sazonov <v.sazonov@kefirgames.com>
2020-10-29 16:01:41 +0400

    add: initialize weapon of game

--- Models/ModelManager.cs
+++ Models/ModelManager.cs
@@ -1,13 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Models.Characters;
+using Models.Weapons;
 
 namespace Models
 {
     public class ModelManager : IModelManager
     {
-        public int ControllablePlayerExemplarId { get; set; }
         public ITrackableDictionary<int, IPlayerModel> PlayerModelDic { get; } = new TrackableDictionary<int, IPlayerModel>();
         public ITrackableDictionary<int, ICharacterModel> CharacterModelDic { get; } = new TrackableDictionary<int, ICharacterModel>();
+        public IList<IWeaponModel> WeaponModelForAllPlayersList = new List<IWeaponModel>();
+
+        public ModelManager()
+        {
+            InstantiateWeapons();
+        }
+
+        private void InstantiateWeapons()
+        {
+            var axeWeaponData = new WeaponData
+            {
+                Id = "axe",
+                Damage = 2
+            };
+            
+            var knifeWeaponData = new WeaponData
+            {
+                Id = "knife",
+                Damage = 6
+            };
+            WeaponModelForAllPlayersList.Add(new WeaponModel(axeWeaponData));
+            WeaponModelForAllPlayersList.Add(new WeaponModel(knifeWeaponData));
+        }
     }
 }
