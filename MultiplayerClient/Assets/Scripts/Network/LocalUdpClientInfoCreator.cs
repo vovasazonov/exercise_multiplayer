6a213e9a2b370e16d52473e25bb5e7d1d61e6304
Vladimir Sazonov <v.sazonov@kefirgames.com>
2020-10-30 11:59:22 +0400

    add: LocalUdpClientInfoCreator.cs

---
+++ MultiplayerClient/Assets/Scripts/Network/LocalUdpClientInfoCreator.cs
@@ -0,0 +1,17 @@
+﻿namespace Network
+{
+    public class LocalUdpClientInfoCreator : IUdpClientInfoCreator
+    {
+        public UdpClientInfo Create()
+        {
+            UdpClientInfo localUdpClientInfo = new UdpClientInfo
+            {
+                ServerIp = "127.0.0.1",
+                ServerPort = 3000,
+                ChannelId = 0
+            };
+
+            return localUdpClientInfo;
+        }
+    }
+}
