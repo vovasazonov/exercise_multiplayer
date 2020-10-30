5521c678af12a2c9a3f4647db6c349b3aba971c9
Vladimir Sazonov <v.sazonov@kefirgames.com>
2020-10-30 11:43:57 +0400

    add: get random number extension

---
+++ Server/System/RandomExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace System
+{
+    public static class RandomExtension
+    {
+        public static int NextExclude(this Random random, IEnumerable<int> exclude)
+        {
+            var excludeHash = new HashSet<int>(exclude);
+            var randomNumber = random.Next();
+            while (excludeHash.Contains(randomNumber))
+            {
+                randomNumber = random.Next();
+            }
+
+            return randomNumber;
+        }
+    }
+}
