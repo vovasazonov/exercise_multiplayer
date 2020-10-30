c40b4e655167fc4be0e5e0deb5e5bf1fb94113f4
Vladimir Sazonov <v.sazonov@kefirgames.com>
2020-10-30 11:42:42 +0400

    fix: absent event in interface

--- Models/Characters/ICharacterModel.cs
+++ Models/Characters/ICharacterModel.cs
@@ -6,6 +6,8 @@ namespace Models.Characters
     public interface ICharacterModel
     {
         event EventHandler<AttackEventArgs> EnemyAttacked; 
+        event EventHandler HoldWeaponChanged; 
+
         string Id { get; }
         IHealthPointModel HealthPoint { get; }
         IWeaponModel HoldWeapon { get; }
