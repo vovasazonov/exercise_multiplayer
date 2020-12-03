89a4decf3feb03192a1beb58de7b49b4661e1915
Vladimir <48253536+vovasazonov@users.noreply.github.com>
2020-12-03 16:04:27 +0400

    add: primitive property

---
+++ Replications/PrimitiveProperty.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Replications
+{
+    public class PrimitiveProperty<T> : IReplication
+    {
+        private T _oldValue;
+        private readonly Func<T> _getDelegate;
+        private readonly Action<object> _setDelegate;
+
+        public PrimitiveProperty(Func<T> getDelegate, Action<object> setDelegate)
+        {
+            _getDelegate = getDelegate;
+            _setDelegate = setDelegate;
+            ResetDiff();
+        }
+
+        public bool ContainsDiff()
+        {
+            return !_oldValue.Equals(_getDelegate.Invoke());
+        }
+
+        public void ResetDiff()
+        {
+            _oldValue = _getDelegate.Invoke();
+        }
+
+        public object WriteDiff()
+        {
+            return _getDelegate.Invoke();
+        }
+
+        public object WriteWhole()
+        {
+            return _getDelegate.Invoke();
+        }
+
+        public void Read(object obj)
+        {
+            _setDelegate.Invoke(obj);
+        }
+    }
+}
