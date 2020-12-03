8982b4bcf5729c4572d7395839c6145f40fae3cc
Vladimir <48253536+vovasazonov@users.noreply.github.com>
2020-12-03 16:14:12 +0400

    add: replication property

---
+++ Replications/ReplicationProperty.cs
@@ -0,0 +1,37 @@
+﻿namespace Replications
+{
+    public class ReplicationProperty : IReplication
+    {
+        private readonly IReplication _replication;
+
+        public ReplicationProperty(IReplication replication)
+        {
+            _replication = replication;
+        }
+        
+        public bool ContainsDiff()
+        {
+            return _replication.ContainsDiff();
+        }
+
+        public void ResetDiff()
+        {
+            _replication.ResetDiff();
+        }
+
+        public object WriteDiff()
+        {
+            return _replication.WriteDiff();
+        }
+
+        public object WriteWhole()
+        {
+            return _replication.WriteWhole();
+        }
+
+        public void Read(object obj)
+        {
+            _replication.WriteWhole();
+        }
+    }
+}
